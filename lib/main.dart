9de58c1e24710f26d9620423bb1fe19508c2a63b
amgadelmarghny <amgadsaad22@gmail.com>
2024-01-25 10:03:34 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const StoreAp());
+}
+
+class StoreAp extends StatelessWidget {
+  const StoreAp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp();
+  }
+}
