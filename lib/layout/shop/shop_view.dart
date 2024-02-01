5f606bfe746e5370cfd70f5aec7fa77108ad78e0
amgadelmarghny <amgadsaad22@gmail.com>
2024-02-01 19:25:47 +0200

    add screns to drower

--- lib/layout/shop/shop_view.dart
+++ lib/layout/shop/shop_view.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:store_2/mdules/categories/categories_view.dart';
-import 'package:store_2/mdules/favorite/favorite_view.dart';
 import 'package:store_2/shared/bloc/shop_cubit/shop_cubit.dart';
 import 'package:store_2/shared/style/colors.dart';
 
@@ -11,46 +9,13 @@ class ShopView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    List<Widget> draverItems = [
-      ListTile(
-        leading: const Icon(Icons.shopping_cart_outlined),
-        title: const Text(
-          'Shop',
-        ),
-        onTap: () {
-          Navigator.pushNamed(context, ShopView.id);
-        },
-      ),
-      ListTile(
-        leading: const Icon(Icons.favorite_outline),
-        title: const Text(
-          'Favorite',
-        ),
-        onTap: () {
-          Navigator.pushNamed(context, FavoriteView.id);
-        },
-      ),
-      ListTile(
-        leading: const Icon(Icons.category_outlined),
-        title: const Text(
-          'Categories',
-        ),
-        onTap: () {
-          Navigator.pushNamed(context, CategoriesView.id);
-        },
-      ),
-      ListTile(
-        leading: const Icon(Icons.settings_outlined),
-        title: const Text(
-          'Settings',
-        ),
-        onTap: () {},
-      ),
-    ];
     return BlocProvider(
       create: (context) => ShopCubit(),
       child: BlocBuilder<ShopCubit, ShopState>(
         builder: (context, state) {
+          List<Widget> draverItems =
+              BlocProvider.of<ShopCubit>(context).listMenu(context);
+
           return Scaffold(
             drawer: Drawer(
               backgroundColor: defaultColor[300],
@@ -63,6 +28,7 @@ class ShopView extends StatelessWidget {
                   itemCount: draverItems.length),
             ),
             appBar: AppBar(
+            
               title: const Text('Shop'),
             ),
           );
--- lib/shared/bloc/shop_cubit/shop_cubit.dart
+++ lib/shared/bloc/shop_cubit/shop_cubit.dart
@@ -1,10 +1,80 @@
 import 'package:bloc/bloc.dart';
 import 'package:flutter/material.dart';
-
+import 'package:store_2/layout/shop/shop_view.dart';
+import 'package:store_2/mdules/categories/categories_view.dart';
+import 'package:store_2/mdules/favorite/favorite_view.dart';
+import 'package:store_2/shared/style/colors.dart';
 
 part 'shop_state.dart';
 
 class ShopCubit extends Cubit<ShopState> {
   ShopCubit() : super(ShopInitial());
-   
+  List<Widget> listMenu(context) {
+    List<Widget> draverItems;
+    return draverItems = [
+      ListTile(
+        leading: const Icon(Icons.shopping_cart_outlined),
+        title: const Text(
+          'Shop',
+        ),
+        onTap: () {
+          Navigator.pushNamed(context, ShopView.id);
+        },
+      ),
+      ListTile(
+        leading: const Icon(Icons.favorite_outline),
+        title: const Text(
+          'Favorite',
+        ),
+        onTap: () {
+          Navigator.pushNamed(context, FavoriteView.id);
+        },
+      ),
+      ListTile(
+        leading: const Icon(Icons.category_outlined),
+        title: const Text(
+          'Categories',
+        ),
+        onTap: () {
+          Navigator.pushNamed(context, CategoriesView.id);
+        },
+      ),
+      PopupMenuButton(
+        position: PopupMenuPosition.under,
+        color: defaultColor[200],
+        onSelected: (value) {
+          if (value == 1) {
+          } else if (value == 2) {}
+        },
+        itemBuilder: (context) => [
+          PopupMenuItem(
+            value: 1,
+            child: ListTile(
+              title: Text(
+                'Britness (Light , Dark)',
+                style: Theme.of(context).textTheme.bodyLarge,
+              ),
+              leading: const Icon(Icons.brightness_6_outlined),
+            ),
+          ),
+          PopupMenuItem(
+            value: 1,
+            child: ListTile(
+              title: Text(
+                'Language (En , Ar)',
+                style: Theme.of(context).textTheme.bodyLarge,
+              ),
+              leading: const Icon(Icons.translate_outlined),
+            ),
+          ),
+        ],
+        child: const ListTile(
+          leading: Icon(Icons.settings_outlined),
+          title: Text(
+            'Settings',
+          ),
+        ),
+      ),
+    ];
+  }
 }
