936833dcca3096ef7aca43406d15a35bbf400e86
amgadelmarghny <amgadsaad22@gmail.com>
2024-02-05 16:33:52 +0200

    refactor login and register input

---
+++ lib/mdules/login/form_input.dart
@@ -0,0 +1,87 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:store_2/shared/bloc/auth_cubit/auth_cubit.dart';
+import 'package:store_2/shared/componants/custom_buttomt.dart';
+import 'package:store_2/shared/componants/icon_auth_list_view.dart';
+import 'package:store_2/shared/componants/textformfield.dart';
+
+Form loginFormInput(
+    GlobalKey<FormState> formKey, BuildContext context, AuthState state) {
+  String? email, passWord;
+  return Form(
+    key: formKey,
+    autovalidateMode: BlocProvider.of<AuthCubit>(context).autovalidateMode,
+    child: Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        CustomTextField(
+          prefixIcon: Icons.email_outlined,
+          onChange: (value) {
+            email = value;
+          },
+          hintText: 'Enter Email',
+          textInputType: TextInputType.emailAddress, labelText:  "Email",
+        ),
+        const SizedBox(
+          height: 20,
+        ),
+        CustomTextField(
+          obscureText: BlocProvider.of<AuthCubit>(context).obscureText,
+          prefixIcon: Icons.lock_outline,
+          onChange: (value) {
+            passWord = value;
+          },
+          hintText: 'Enter Password',
+          textInputType: TextInputType.visiblePassword,
+          suffixIcon: BlocProvider.of<AuthCubit>(context).suffixIcon,
+          suffixOnPressed: () {
+            BlocProvider.of<AuthCubit>(context).onEyesPressed();
+          }, labelText:  "Password" ,
+        ),
+        const SizedBox(
+          height: 20,
+        ),
+        TextButton(
+          onPressed: () {},
+          child: Text(
+            'Forget you password?',
+            style: Theme.of(context).textTheme.bodyMedium,
+          ),
+        ),
+        const SizedBox(
+          height: 50,
+        ),
+        CustomButtomShet(
+          isLoading: state is LoginLodingState ? true : false,
+          text: 'Login',
+          onTap: () {
+            if (formKey.currentState!.validate()) {
+              context
+                  .read<AuthCubit>()
+                  .userLogin(email: email!, passWord: passWord!);
+              formKey.currentState!.save();
+            } else {
+              BlocProvider.of<AuthCubit>(context).validateObserver();
+            }
+          },
+        ),
+        const SizedBox(
+          height: 30,
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Login With',
+              style: Theme.of(context).textTheme.bodyLarge,
+            ),
+          ],
+        ),
+        const SizedBox(
+          height: 30,
+        ),
+        const IconAuthlistView(),
+      ],
+    ),
+  );
+}
--- lib/mdules/register/sliver_register_input.dart
+++ lib/mdules/register/sliver_register_input.dart
@@ -1,13 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:store_2/layout/shop/shop_view.dart';
-import 'package:store_2/models/user_model/user_model.dart';
+import 'package:store_2/mdules/register/form_input.dart';
 import 'package:store_2/shared/bloc/auth_cubit/auth_cubit.dart';
-import 'package:store_2/shared/componants/custom_buttomt.dart';
 import 'package:store_2/shared/componants/custom_show_messeges.dart';
-import 'package:store_2/shared/componants/icon_auth_list_view.dart';
 import 'package:store_2/shared/componants/navigation.dart';
-import 'package:store_2/shared/componants/textformfield.dart';
 import 'package:store_2/shared/network/lockal/key_const.dart';
 import 'package:store_2/shared/network/lockal/shared_helper.dart';
 
@@ -18,7 +15,6 @@ class SliverRegisterInfo extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    String? name, email, phone, password;
     GlobalKey<FormState> formKey = GlobalKey();
     return BlocConsumer<AuthCubit, AuthState>(
       listener: (context, state) async {
@@ -39,7 +35,6 @@ class SliverRegisterInfo extends StatelessWidget {
                 navigatorPushAndRemove(
                   context,
                   ShopView.id,
-                  arguments: state.registermodel.data!.token,
                 );
               },
             );
@@ -60,112 +55,10 @@ class SliverRegisterInfo extends StatelessWidget {
               left: 20,
               right: 20,
             ),
-            child: Form(
-              key: formKey,
-              autovalidateMode:
-                  BlocProvider.of<AuthCubit>(context).autovalidateMode,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  CustomTextField(
-                    prefixIcon: Icons.person_outline,
-                    onChange: (value) {
-                      name = value;
-                    },
-                    hintText: 'Enter Name',
-                    textInputType: TextInputType.name,
-                  ),
-                  const SizedBox(
-                    height: 20,
-                  ),
-                  CustomTextField(
-                    prefixIcon: Icons.email_outlined,
-                    onChange: (value) {
-                      email = value;
-                    },
-                    hintText: ' Enter Email',
-                    textInputType: TextInputType.emailAddress,
-                  ),
-                  const SizedBox(
-                    height: 20,
-                  ),
-                  CustomTextField(
-                    obscureText:
-                        BlocProvider.of<AuthCubit>(context).obscureText,
-                    prefixIcon: Icons.lock_outline,
-                    onChange: (value) {
-                      password = value;
-                    },
-                    hintText: 'Enter Password',
-                    textInputType: TextInputType.visiblePassword,
-                    suffixIcon: BlocProvider.of<AuthCubit>(context).suffixIcon,
-                    suffixOnPressed: () {
-                      BlocProvider.of<AuthCubit>(context).onEyesPressed();
-                    },
-                  ),
-                  const SizedBox(
-                    height: 20,
-                  ),
-                  CustomTextField(
-                    prefixIcon: Icons.phone_outlined,
-                    onChange: (value) {
-                      phone = value;
-                    },
-                    hintText: 'Enter Phone Number',
-                    textInputType: TextInputType.phone,
-                  ),
-                  const SizedBox(
-                    height: 30,
-                  ),
-                  CustomButtomShet(
-                    text: 'SIGN UP',
-                    isLoading: state is RegisterLodingState ? true : false,
-                    onTap: () {
-                      onTapMethod(
-                        formKey,
-                        name,
-                        email,
-                        password,
-                        phone,
-                        context,
-                      );
-                    },
-                  ),
-                  const SizedBox(
-                    height: 20,
-                  ),
-                  Center(
-                    child: Text(
-                      'Sign up With',
-                      style: Theme.of(context).textTheme.bodyLarge,
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 20,
-                  ),
-                  const IconAuthlistView(),
-                ],
-              ),
-            ),
+            child: registerFormInput(formKey, context, state),
           ),
         );
       },
     );
-  }
-
-  void onTapMethod(GlobalKey<FormState> formKey, String? name, String? email,
-      String? password, String? phone, BuildContext context) {
-    if (formKey.currentState!.validate()) {
-      formKey.currentState!.save();
-      UserModel userModel = UserModel(
-        name: name!,
-        email: email!,
-        password: password!,
-        phone: phone!,
-      );
-      BlocProvider.of<AuthCubit>(context).userRegister(userModel: userModel);
-    } else {
-      BlocProvider.of<AuthCubit>(context).validateObserver();
-    }
   }
 }
