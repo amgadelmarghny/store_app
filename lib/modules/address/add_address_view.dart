74f50e6bbcf660d5069656b8a86b04289ec4af28
amgadelmarghny <amgadsaad22@gmail.com>
2024-03-26 01:19:27 +0200

    add add address view

---
+++ lib/modules/address/add_address_view.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:store_2/modules/address/add_address_view_body.dart';
+import 'package:store_2/shared/bloc/address_cubit/address_cubit.dart';
+
+class AddAddressView extends StatelessWidget {
+  const AddAddressView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider(
+      create: (context) => AddressCubit(),
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('Address Details'),
+        ),
+        body: const AddAddressViewBody(),
+      ),
+    );
+  }
+}
---
+++ lib/modules/address/add_address_view_body.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class AddAddressViewBody extends StatelessWidget {
+  const AddAddressViewBody({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 20),
+      child: Column(children: [
+        
+      ]),
+    );
+  }
+}
