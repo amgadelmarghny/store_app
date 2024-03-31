35ec2ec093297a94860fa5af6981c6f69fbee38d
amgadelmarghny <amgadsaad22@gmail.com>
2024-03-31 23:38:49 +0200

    create update model

---
+++ lib/models/address_models/update_address_model.dart
@@ -0,0 +1,37 @@
+class UpdateAddressModel {
+  bool? status;
+  String? message;
+  UpdateAddressDataModel? data;
+
+  UpdateAddressModel({this.status, this.message, this.data});
+
+  UpdateAddressModel.fromJson(Map<String, dynamic> json) {
+    status = json['status'];
+    message = json['message'];
+    data = json['data'] != null
+        ? UpdateAddressDataModel.fromJson(json['data'])
+        : null;
+  }
+}
+
+class UpdateAddressDataModel {
+  int? id;
+  dynamic name;
+  dynamic city;
+  dynamic region;
+  dynamic details;
+  dynamic notes;
+  double? latitude;
+  double? longitude;
+
+  UpdateAddressDataModel.fromJson(Map<String, dynamic> json) {
+    id = json['id'];
+    name = json['name'];
+    city = json['city'];
+    region = json['region'];
+    details = json['details'];
+    notes = json['notes'];
+    latitude = json['latitude'];
+    longitude = json['longitude'];
+  }
+}
