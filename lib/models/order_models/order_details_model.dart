e63eb83d394896a3e68932ff2df6c3d4a7e86c31
amgadelmarghny <amgadsaad22@gmail.com>
2024-04-07 01:32:42 +0200

    create order details model

---
+++ lib/models/order_models/order_details_model.dart
@@ -0,0 +1,69 @@
+import 'package:store_2/models/address_models/address_model.dart';
+
+class OrderDetailsModel {
+  bool? status;
+  String? message;
+  Data? data;
+
+  OrderDetailsModel({this.status, this.message, this.data});
+
+  OrderDetailsModel.fromJson(Map<String, dynamic> json) {
+    status = json['status'];
+    message = json['message'];
+    data = json['data'] != null ? Data.fromJson(json['data']) : null;
+  }
+}
+
+class Data {
+  int? id;
+  int? cost;
+  int? discount;
+  double? points;
+  double? vat;
+  double? total;
+  int? pointsCommission;
+  String? promoCode;
+  String? paymentMethod;
+  String? date;
+  String? status;
+  AddressModel? addressModel;
+  List<Products>? products;
+
+  Data.fromJson(Map<String, dynamic> json) {
+    id = json['id'];
+    cost = json['cost'];
+    discount = json['discount'];
+    points = json['points'];
+    vat = json['vat'];
+    total = json['total'];
+    pointsCommission = json['points_commission'];
+    promoCode = json['promo_code'];
+    paymentMethod = json['payment_method'];
+    date = json['date'];
+    status = json['status'];
+    addressModel =
+        json['address'] != null ? AddressModel.fromJson(json['address']) : null;
+    if (json['products'] != null) {
+      products = <Products>[];
+      json['products'].forEach((v) {
+        products!.add(Products.fromJson(v));
+      });
+    }
+  }
+}
+
+class Products {
+  int? id;
+  int? quantity;
+  int? price;
+  String? name;
+  String? image;
+
+  Products.fromJson(Map<String, dynamic> json) {
+    id = json['id'];
+    quantity = json['quantity'];
+    price = json['price'];
+    name = json['name'];
+    image = json['image'];
+  }
+}
