6fefa2913b52d0d4d4da6063dac7146aaac389dc
Amgad Elmarghny <amgadsaad22@gmail.com>
2024-04-10 04:16:45 +0200

    edit address view

--- lib/modules/order/order_item/components/address_view.dart
+++ lib/modules/order/order_item/components/address_view.dart
@@ -57,52 +57,5 @@ class AddressView extends StatelessWidget {
         ),
       ],
     );
-
-    return Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-      const Text('Add your address'),
-      const SizedBox(height: 30),
-      Row(
-        children: [
-          AddressField(
-            width: 110,
-            isOrderView: true,
-            hintText: 'address name',
-            messageValidationName: 'Address name',
-            textEditingController: nameController,
-          ),
-          const Spacer(),
-          AddressField(
-            width: 120,
-            isOrderView: true,
-            hintText: 'city',
-            messageValidationName: 'City',
-            textEditingController: cityController,
-          ),
-        ],
-      ),
-      const SizedBox(height: 20),
-      AddressField(
-        hintText: 'region',
-        isOrderView: true,
-        messageValidationName: 'Region',
-        textEditingController: regionController,
-      ),
-      const SizedBox(height: 20),
-      AddressField(
-        hintText: 'details',
-        isOrderView: true,
-        messageValidationName: 'Details',
-        textEditingController: detailsController,
-      ),
-      const SizedBox(height: 20),
-      AddressField(
-        isRequired: false,
-        hintText: 'notes',
-        maxLine: 5,
-        isOrderView: true,
-        messageValidationName: 'Notes',
-        textEditingController: notesController,
-      ),
-    ]);
   }
 }
