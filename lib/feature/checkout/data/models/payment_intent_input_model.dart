4af9a94004ea20c500103eb87969782a956d00ca
Amgad Elmarghny <amgadsaad22@gmail.com>
2024-04-21 08:50:10 +0200

    create patment intent input method

---
+++ lib/feature/checkout/data/models/payment_intent_input_model.dart
@@ -0,0 +1,13 @@
+class PaymentIntentInputModel {
+  final String currency;
+  final String amount;
+
+  PaymentIntentInputModel({required this.currency, required this.amount});
+
+  Map<String, dynamic> toJson() {
+    return {
+      'currency': currency,
+      'amount': amount,
+    };
+  }
+}
