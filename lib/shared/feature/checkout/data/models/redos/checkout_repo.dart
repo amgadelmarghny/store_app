eeb68d3e81ee016f3a2b1688486381b8f98b273c
Amgad Elmarghny <amgadsaad22@gmail.com>
2024-04-23 19:44:55 +0200

    create checkout repo

---
+++ lib/shared/feature/checkout/data/models/redos/checkout_repo.dart
@@ -0,0 +1,9 @@
+import 'package:dartz/dartz.dart';
+import 'package:store_2/shared/errors/errors.dart';
+import 'package:store_2/shared/feature/checkout/data/models/payment_intent_input_model.dart';
+
+abstract class CheckoutRedo {
+  
+  Future<Either<Failure, void>> makePayment(
+      {required PaymentIntentInputModel paymentIntentInputModel});
+}
---
+++ lib/shared/feature/checkout/data/models/redos/checkout_repo_empl.dart
@@ -0,0 +1,19 @@
+import 'package:dartz/dartz.dart';
+import 'package:store_2/shared/errors/errors.dart';
+import 'package:store_2/shared/feature/checkout/data/models/payment_intent_input_model.dart';
+import 'package:store_2/shared/feature/checkout/data/models/redos/checkout_repo.dart';
+import 'package:store_2/shared/network/remot/stripe_service.dart';
+
+class CheckoutRepoImpl implements CheckoutRedo {
+  @override
+  Future<Either<Failure, void>> makePayment(
+      {required PaymentIntentInputModel paymentIntentInputModel}) async {
+    try {
+      await StripeService.makePayment(
+          paymentIntentInputModel: paymentIntentInputModel);
+      return right(null);
+    } catch (e) {
+      return left(ServerFailure(errMessage: e.toString()));
+    }
+  }
+}
